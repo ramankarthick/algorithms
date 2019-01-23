70181faf5b5b54a3f1f7f8775fc78ab712ccb78e
ramankarthick <35781659+ramankarthick@users.noreply.github.com>
2019-01-22 19:12:14 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,4 @@
-public class MyClass {
+public class BinarySearch {
     public static void main(String args[]) {
        int array[] = {1,2,3,4,5,6,7,8,9,10};
         int index = binarySearch(array, 11);   
